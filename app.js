50e2e55cfc3bd14da9a915c58b298bfb10fcfc34
Scott Brunsma <lmbrunsma@gmail.com>
2026-04-14 11:02:11 -0700

    added #32

--- app.js
+++ app.js
@@ -1,39 +1,42 @@
 const store = require('./dataStore');
 const express = require('express');
 const path = require('path');
-
 const app = express();
 const PORT = 3000;
 
-//Add user
-store.add('users.json', {
-    id: 1,
-    username: 'scott'
-});
-
-//Add event
-store.add('events.json', {
-    id: 1,
-    title: 'Meet up with Rebecca'
-});
-
-//Get one user
-const user = store.getOne('users.json', 'username', 'scott');
-console.log(user);
-
-//Get all events
-const events = store.getAll('events.json');
-console.log(events);
+app.use(express.json());
 
 // Serve static files
 app.use('/css', express.static(path.join(__dirname, 'css')));
 app.use('/js', express.static(path.join(__dirname, 'js')));
 
-// Basic route 
+// Home
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'static-content', 'index.html'));
+  res.sendFile(path.join(__dirname, 'static-content', 'index.html'));
+});
+
+// Login page
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'static-content', 'login.html'));
+});
+
+// Login form submission
+app.post('/user/login', (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.json({ success: false, message: 'Username and password are required.' });
+  }
+
+  const user = store.getOne('users.json', 'username', username);
+
+  if (!user || user.passwordHash !== password) {
+    return res.json({ success: false, message: 'Invalid username or password.' });
+  }
+
+  res.json({ success: true, message: 'Logged in.' });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
